0431111d977d904584db0348108dcb0f74f1bde5
Rizwan Mallal <rmallal@gmail.com>
2019-09-18 20:24:50 -0400

    Updated docker-topicm-plato-republic-50 to build from v2 code base that is based on quanta_main.py

--- engines/plato-republic-50/topicm/quanta_setmodel.py
+++
@@ -1,6 +0,0 @@
-#
-# Only place where you set the model name.
-# Look at api2vec_pretrained_modelinfo to get the list of names
-#
-from quanta_pretrained_modelinfo import *
-MODEL_OF_INTEREST=REPUBLIC_50_TMODEL_TP_10
