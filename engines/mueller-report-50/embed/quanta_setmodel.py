dd333ef3beb92a3d2b441ea19b1610cb5b110140
Rizwan Mallal <rmallal@gmail.com>
2019-09-20 17:43:11 -0400

    Changed the file structure. Introduced docker subdir

--- engines/mueller-report-50/embed/quanta_setmodel.py
+++
@@ -1,6 +0,0 @@
-#
-# Only place where you set the model name.
-# Look at api2vec_pretrained_modelinfo to get the list of names
-#
-from quanta_pretrained_modelinfo import *
-MODEL_OF_INTEREST=MUELLER_REPORT_50_MODEL
